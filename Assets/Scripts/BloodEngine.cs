3190b547c24a6c6856d47e2b95cde09ae6307c84
Simo Pöllänen <Simo Pllnen>
2016-01-29 21:03:19 +0200

    Added first rev of blood engine

---
+++ Assets/Scripts/BloodEngine.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BloodEngine : MonoBehaviour {
+
+    List<BloodScript> bloodPool = new List<BloodScript>();
+    List<BloodScript> activeBlood = new List<BloodScript>();
+    public List<GameObject> baseBloodSplatters = new List<GameObject>();
+
+    GameObject BloodSplatters;
+    public int maxAmountOfBlood = 1000;
+
+
+    
+	// Use this for initialization
+	void Start () {
+        BloodSplatters = new GameObject("BloodSplatters");
+        Transform bst = BloodSplatters.GetComponent<Transform>();
+        Vector3 spawnPos = new Vector3(0, -100, 0);
+        //Fill the blood pool
+        for (int a = 0; a < maxAmountOfBlood; ++a)
+        {
+            GameObject go = (GameObject)Instantiate(baseBloodSplatters[0], spawnPos, Quaternion.identity);
+            go.transform.parent = bst;
+            bloodPool.Add(go.GetComponent<BloodScript>());
+            bloodPool[a].Freeze();
+        }
+
+    }
+
+    void ReserveBlood(int amount)
+    {
+        int reserveAmount = amount;
+        if (bloodPool.Count < amount)
+        {
+            if (maxAmountOfBlood < reserveAmount)
+                reserveAmount = maxAmountOfBlood;
+            FreeBlood(reserveAmount - bloodPool.Count);
+        }
+        for (int a = 0; a < reserveAmount; ++a)
+        {
+            bloodPool[a].active = true;
+            activeBlood.Add(bloodPool[a]);
+        }
+        bloodPool.RemoveRange(0, reserveAmount);
+    }
+
+    void FreeBlood(int amount)
+    {
+        int freeAmount = amount;
+        if (activeBlood.Count < amount)
+        {
+            freeAmount = activeBlood.Count;
+        }
+        for (int a = 0; a < amount; ++a)
+        {
+            activeBlood[a].active = false;
+            bloodPool.Add(activeBlood[a]);   
+        }
+        activeBlood.RemoveRange(0, freeAmount);
+       
+    }
+    Vector3 AddVariation(Vector3 src, Vector3 variance)
+    {
+        Vector3 output = src;
+        output.x = output.x + Random.Range(-variance.x, variance.x);
+        output.y = output.y + Random.Range(-variance.y, variance.y);
+        output.z = output.z + Random.Range(-variance.z, variance.z);
+        return output;
+    }
+
+    public void CreateTestSplash(int testVal)
+    {
+        switch (testVal)
+        {
+            case 0:
+                CreateSplash(new Vector3(0,2,0), Vector3.forward, new Vector3(200,200,1), 200, 30);
+                break;
+            case 1:
+                CreateSplash(Vector3.zero, Vector3.forward, new Vector3(10, 10, 10), 2, 100);
+                break;
+            default:
+                CreateSplash(Vector3.zero, Vector3.forward, new Vector3(10, 10, 10), 2, 500);
+                break;
+        }
+    }
+
+    public void CreateSplash(Vector3 startPos, Vector3 direction, Vector3 variance, float startSpeed, int amount)
+    {
+        int bloodAmount = amount;
+        if (maxAmountOfBlood < amount)
+            bloodAmount = maxAmountOfBlood;
+        ReserveBlood(bloodAmount);
+        int targetEntity = 0;
+        Vector3 cameraLookDirection = Camera.main.transform.forward;
+        Vector3 normalizedDirection = direction.normalized;
+        for (int a = 0; a < bloodAmount; ++a)
+        {
+            targetEntity = activeBlood.Count - bloodAmount + a;
+            activeBlood[targetEntity].SetPosition(startPos);
+            activeBlood[targetEntity].SetRotation(Quaternion.LookRotation(cameraLookDirection));
+            activeBlood[targetEntity].AddForce(AddVariation(normalizedDirection*startSpeed, variance));
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/BloodScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloodScript : MonoBehaviour {
+
+    Transform t;
+    Rigidbody r;
+    public bool active = false;
+    bool needToFreeze = false;
+
+	// Use this for initialization
+	void Start () {
+        t = GetComponent<Transform>();
+        r = GetComponent<Rigidbody>();
+	}
+
+    public void SetPosition(Vector3 worldPos)
+    {
+        t.position = worldPos;
+        r.velocity = Vector3.zero;
+        r.angularVelocity = Vector3.zero;
+    }
+    public void SetRotation(Quaternion rotation)
+    {
+        t.rotation = rotation;
+    }
+    public void AddForce(Vector3 force)
+    {
+        //r.velocity = force;
+        r.isKinematic = false;
+        r.useGravity = true;
+        r.AddForce(force, ForceMode.Force);
+
+    }
+    public void Freeze()
+    {
+        needToFreeze = true;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (needToFreeze)
+        {
+            r.isKinematic = true;
+            r.useGravity = false;
+            needToFreeze = false;
+        }
+	}
+
+    void OnCollisionEnter(Collision coll)
+    {
+        //ContactPoint contact = coll.contacts[0];
+        //Quaternion rot = Quaternion.FromToRotation(-Camera.main.transform.forward, contact.normal);
+        
+        //SetRotation(rot);
+        //Freeze();
+    }
+}
