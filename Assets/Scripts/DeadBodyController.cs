c69614bcf41b42557ab520b81c39281f7c633701
Mikko Romppainen <kajakbros@gmail.com>
2016-01-30 23:28:44 +0200

    Added dead body controller.

---
+++ Assets/Scripts/DeadBodyController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadBodyController : MonoBehaviour, ActionInterface
+{
+    public float impulse;
+
+    float m_cooldown;
+
+    public void Start()
+    {
+        m_cooldown = 0.0f;
+    }
+
+    public void Update()
+    {
+        m_cooldown -= Time.deltaTime;
+    }
+
+    public void doAction(GameObject other)
+    {
+        if( m_cooldown < 0.0f )
+        {
+            Rigidbody body = GetComponent<Rigidbody>();
+            if (body != null)
+            {
+                Vector3 delta = gameObject.transform.position - other.transform.position;
+                delta.Normalize();
+                body.AddExplosionForce(impulse,
+                    other.transform.position, 1.0f, 1.0f, ForceMode.Impulse);
+                m_cooldown = 0.5f;
+            }
+        }
+    }
+}
