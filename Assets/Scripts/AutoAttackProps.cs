a60e19369181ea9ce2b5a92d4332607f72315f31
Mikko Romppainen <kajakbros@gmail.com>
2016-01-30 22:07:46 +0200

    Added AutoAttackProps -script.

---
+++ Assets/Scripts/AutoAttackProps.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoAttackProps : MonoBehaviour {
+
+    DynamicPropsController[] m_actionInterfaces = null;
+
+    private void findDynamicPropsControllers(Collider other)
+    {
+        DynamicPropsController[] res = other.gameObject.GetComponentsInChildren<DynamicPropsController>();
+        if (res.Length > 0)
+            m_actionInterfaces = res;
+    }
+
+    void Update()
+    {
+        if( m_actionInterfaces != null )
+        {
+            for (int i = 0; i < m_actionInterfaces.Length; ++i)
+            {
+                m_actionInterfaces[i].doAction();
+            }
+
+            m_actionInterfaces = null;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        findDynamicPropsControllers(other);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        findDynamicPropsControllers(other);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        m_actionInterfaces = null;
+    }
+}
