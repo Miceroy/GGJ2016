df05b1b6e4b9036da5ce2c2f0114a9b1a9003e84
Simo Pöllänen <Simo Pllnen>
2016-01-30 13:39:35 +0200

    Added a simple WP system

--- Assets/Scripts/CustomAIMoveScript.cs
+++ Assets/Scripts/CustomAIMoveScript.cs
@@ -1,72 +1,112 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomAIMoveScript : MonoBehaviour
 {
+    public List<GameObject> waypoints = new List<GameObject>();
 
-        public Vector3 m_move;
-        private Transform player;
-        Transform t;
+    public bool useDefaultWaypoints = true;
+    public int waypointIndex = 0;
+    public float moveSpeed = 2.0f;
+    float waypointActivationRange = 0.1f;
+
+    public Vector3 m_move;
+    private Transform player;
+    Transform t;
+    
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        t = GetComponent<Transform>();
+    }
+
+
+    private void Update()
+    {
         
-        private void Start()
+    }
+
+    
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        RaycastHit rh;
+        //Some layer test code, futher inspection required
+        int omitPoliceLayerMask = 1 << 9;
+        omitPoliceLayerMask = ~omitPoliceLayerMask;
+
+        omitPoliceLayerMask = -1;
+        //Overrode it to look thru all layers
+
+
+        Vector3 targetPos = player.position;
+        if (Physics.Raycast(t.position, Vector3.Normalize(targetPos - t.position), out rh, 80))
         {
-            player = GameObject.FindGameObjectWithTag("Player").transform;
-            t = GetComponent<Transform>();
+            if (rh.collider.gameObject.tag == "Player")
+            {
+                useDefaultWaypoints = false;
+            }
         }
-
-
-        private void Update()
+        if (useDefaultWaypoints)
         {
+            
+                targetPos = waypoints[waypointIndex].transform.position;
+            if (Vector3.Distance(targetPos, t.position) <= waypointActivationRange )
+            {
+                waypoints[waypointIndex].GetComponent<Waypoint>().StartTimer();
+                if (waypoints[waypointIndex].GetComponent<Waypoint>().IsDone())
+                {
+                    ++waypointIndex;
+                    if (waypointIndex >= waypoints.Count)
+                    {
+                        waypointIndex = 0;
+                    }
+                    targetPos = waypoints[waypointIndex].transform.position;
+                }
+            }
             
         }
 
+
+        m_move = (targetPos - t.position).normalized;
+        m_move.Normalize();
+
+   
         
-        // Fixed update is called in sync with physics
-        private void FixedUpdate()
+        if (Physics.Raycast(t.position, m_move, out rh, 80))
         {
-            m_move = (player.position - t.position).normalized;
-            m_move.Normalize();
-
-        //Some layer test code, futher inspection required
-            int omitPoliceLayerMask = 1 << 9;
-            omitPoliceLayerMask = ~omitPoliceLayerMask;
-
-            omitPoliceLayerMask = -1;
-        //Overrode it to look thru all layers
-            RaycastHit rh;
-            if (Physics.Raycast(t.position, m_move, out rh, 80))
+            if (rh.collider.gameObject.tag == "Player")
+            { useDefaultWaypoints = false; }
+            else
             {
-                if (rh.collider.gameObject.tag == "Player")
-                { }
-                else
+                if (Physics.Raycast(t.position, t.forward, out rh, 80, omitPoliceLayerMask))
                 {
-                    if (Physics.Raycast(t.position, t.forward, out rh, 80, omitPoliceLayerMask))
-                    {
-                        if (rh.transform != t)
-                            m_move += rh.normal * 50;
-                    }
+                    if (rh.transform != t)
+                        m_move += rh.normal * 50;
+                }
 
-                    Vector3 l_ray = t.position + new Vector3(-2.0f, 0, 0);
-                    Vector3 r_ray = t.position + new Vector3(2.0f, 0, 0);
-                    if (Physics.Raycast(l_ray, t.forward, out rh, 80, omitPoliceLayerMask))
-                    {
-                        if (rh.transform != t)
-                            m_move += rh.normal * 50;
-                    }
+                Vector3 l_ray = t.position + new Vector3(-2.0f, 0, 0);
+                Vector3 r_ray = t.position + new Vector3(2.0f, 0, 0);
+                if (Physics.Raycast(l_ray, t.forward, out rh, 80, omitPoliceLayerMask))
+                {
+                    if (rh.transform != t)
+                        m_move += rh.normal * 50;
+                }
 
-                    if (Physics.Raycast(r_ray, t.forward, out rh, 80, omitPoliceLayerMask))
-                    {
-                        if (rh.transform != t)
-                            m_move += rh.normal * 50;
-                    }
+                if (Physics.Raycast(r_ray, t.forward, out rh, 80, omitPoliceLayerMask))
+                {
+                    if (rh.transform != t)
+                        m_move += rh.normal * 50;
                 }
             }
-
-
-            m_move.y = 0;
-            Quaternion q = Quaternion.LookRotation(m_move);
-            t.rotation = Quaternion.Slerp(t.rotation, q, Time.deltaTime * 20);
-            t.position += t.forward * 3 * Time.deltaTime;
-
         }
+
+
+        m_move.y = 0;
+        Quaternion q = Quaternion.LookRotation(m_move);
+        t.rotation = Quaternion.Slerp(t.rotation, q, Time.deltaTime * 20);
+        t.position += t.forward * 3 * Time.deltaTime;
+
+    }
 }
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour
+{
+    public float stayTime = 0;
+    // Use this for initialization
+    bool timerActive = false;
+    public void StartTimer()
+    {
+        timerActive = true;
+    }
+
+    public bool IsDone()
+    {
+        if (stayTime <= 0)
+        {
+            timerActive = false;
+            return true;
+        }
+        return false;
+    }
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timerActive)
+            stayTime -= Time.deltaTime;
+	
+	}
+}
