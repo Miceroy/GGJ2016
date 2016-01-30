25add26a4bbec2d8e76229fb55102b0c28d875b4
Simo Pöllänen <Simo Pllnen>
2016-01-30 22:40:59 +0200

    Took off some unneeded stuff from ze script, added new simplified anim controller for the Bobbies

--- Assets/Scripts/CustomAIMoveScript.cs
+++ Assets/Scripts/CustomAIMoveScript.cs
@@ -30,6 +30,7 @@ public class CustomAIMoveScript : MonoBehaviour
         brains = GetComponent<NavMeshAgent>();
         m_Animator = GetComponent<Animator>();
         prevPos = t.position;
+        m_Animator.applyRootMotion = true;
     }
 
 
@@ -89,6 +90,7 @@ public class CustomAIMoveScript : MonoBehaviour
         float m_TurnAmount = Mathf.Atan2(move.x, move.z);
         m_Animator.SetFloat("Forward", move.z, 0.1f, Time.deltaTime);
         m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
+        
         //m_Animator.SetBool("Crouch", m_Crouching);
         //m_Animator.SetBool("OnGround", m_IsGrounded);
         //if (!m_IsGrounded)
@@ -99,9 +101,9 @@ public class CustomAIMoveScript : MonoBehaviour
         // calculate which leg is behind, so as to leave that leg trailing in the jump animation
         // (This code is reliant on the specific run cycle offset in our animations,
         // and assumes one leg passes the other at the normalized clip times of 0.0 and 0.5)
-        float runCycle =
-            Mathf.Repeat(
-                m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
+        //float runCycle =
+         //   Mathf.Repeat(
+         //       m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
         //float jumpLeg = (runCycle < k_Half ? 1 : -1) * m_ForwardAmount;
         //if (m_IsGrounded)
         //{
@@ -110,14 +112,14 @@ public class CustomAIMoveScript : MonoBehaviour
 
         // the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
         // which affects the movement speed because of the root motion.
-        if (/*m_IsGrounded &&*/ move.magnitude > 0)
-        {
+        //if (/*m_IsGrounded &&*/ move.magnitude > 0)
+        //{
             m_Animator.speed = animSpeed;
-        }
-        else
-        {
+        //}
+        //else
+        //{
             // don't use that while airborne
-            m_Animator.speed = 1;
-        }
+        //    m_Animator.speed = 1;
+        //}
     }
 }
