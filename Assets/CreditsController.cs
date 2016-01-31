4a8dfdcd5f799438520352c91821fdab4cc3774a
Mikko Romppainen <kajakbros@gmail.com>
2016-01-31 16:11:17 +0200

    Added credits scene

---
+++ Assets/CreditsController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsController : MonoBehaviour {
+
+	// Use this for initialization
+	public void BackToMainMenu()
+    {
+        Application.LoadLevel(0);        
+    }
+}
