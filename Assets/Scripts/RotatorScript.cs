99c3a516574ef31125b491d9f87d2e71265fc174
Mikko Romppainen <kajakbros@gmail.com>
2016-01-31 13:28:33 +0200

    up

--- Assets/Scripts/RotatorScript.cs
+++ Assets/Scripts/RotatorScript.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class RotatorScript : MonoBehaviour {
-
+    Vector3 rotateAxis = new Vector3(0.0f,1.0f,0.0f);
+    float rotateSpeed = 360.0f;
+  //  float 
 	// Use this for initialization
 	void Start () {
 	
