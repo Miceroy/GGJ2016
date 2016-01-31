3ab7bfd8f9f01e9d40f2d4b4aa527c29e8aceac3
Mikko Romppainen <kajakbros@gmail.com>
2016-01-31 16:06:00 +0200

    Credits added to main menu.

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -7,6 +7,11 @@ public class MainMenuController : MonoBehaviour {
         //Debug.Log("Start game clicked");
         Application.LoadLevel(1);
     }
+    void ShowCredits()
+    {
+        //Debug.Log("Start game clicked");
+        Application.LoadLevel(2);
+    }
 
     void Exit()
     {
